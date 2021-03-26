973c75cb3d4f183325558351dd5b29e0bfdb9218
BabyDragon003 <golddragon01234@gmail.com>
2021-03-26 17:21:52 +0900

    "2021-03-26 17:21"

--- packages/classic_terraswap/src/querier.rs
+++ packages/classic_terraswap/src/querier.rs
@@ -13,6 +13,22 @@ use cw20::{BalanceResponse as Cw20BalanceResponse, Cw20QueryMsg, TokenInfoRespon
 pub fn query_balance(
     querier: &QuerierWrapper<TerraQuery>,
     account_addr: Addr,
+    denom: String,
+) -> StdResult<Uint128> {
+    // load price form the oracle
+    let balance: BalanceResponse = querier.query(&QueryRequest::Bank(BankQuery::Balance {
+        address: account_addr.to_string(),
+        denom,
+    }))?;
+    Ok(balance.amount.amount)
+}
+
+pub fn query_all_balances(
+    querier: &QuerierWrapper<TerraQuery>,
+    account_addr: Addr,
+) -> StdResult<Vec<Coin>> {
+    // load price form the oracle
+    let all_balances: AllBalanceResponse =
         querier.query(&QueryRequest::Bank(BankQuery::AllBalances {
             address: account_addr.to_string(),
         }))?;
