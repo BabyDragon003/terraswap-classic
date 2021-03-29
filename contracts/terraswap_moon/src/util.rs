273b7ebb244a8a6596f2b37ae4090c1ae4f13d51
BabyDragon003 <golddragon01234@gmail.com>
2021-03-29 17:21:52 +0900

    "2021-03-29 17:21"

--- contracts/terraswap_moon/src/util.rs
+++ contracts/terraswap_moon/src/util.rs
@@ -1,4 +1,3 @@
-use crate::error::ContractError;
 use cosmwasm_std::{
     to_binary, Addr, BalanceResponse as NativeBalanceResponse, BankMsg, BankQuery, Coin, CosmosMsg,
     QuerierWrapper, QueryRequest, Uint128, WasmMsg, WasmQuery,
