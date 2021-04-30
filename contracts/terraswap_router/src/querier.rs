7b4a7c1cc5327dd106c34d51940bc6be592cf63f
BabyDragon003 <golddragon01234@gmail.com>
2021-04-30 17:21:52 +0900

    "2021-04-30 17:21"

--- contracts/terraswap_router/src/querier.rs
+++ contracts/terraswap_router/src/querier.rs
@@ -18,24 +18,3 @@ pub fn compute_tax(
             DECIMAL_FRACTION,
             DECIMAL_FRACTION * tax_rate + DECIMAL_FRACTION,
         ))?,
-        tax_cap,
-    ))
-}
-
-pub fn compute_reverse_tax(
-    querier: &QuerierWrapper<TerraQuery>,
-    amount: Uint128,
-    denom: String,
-) -> StdResult<Uint128> {
-    let terra_querier = TerraQuerier::new(querier);
-    let tax_rate: Decimal = (terra_querier.query_tax_rate()?).rate;
-    let tax_cap: Uint128 = (terra_querier.query_tax_cap(denom)?).cap;
-
-    let tax: Uint128 = (std::cmp::min(
-        Uint256::from(amount).mul(Decimal256::one() + Decimal256::from(tax_rate)),
-        Uint256::from(amount + tax_cap),
-    ) - Uint256::from(amount))
-    .try_into()?;
-
-    Ok(tax)
-}
--- packages/classic_terraswap/src/util.rs
+++ packages/classic_terraswap/src/util.rs
@@ -13,16 +13,11 @@ pub fn assert_deadline(blocktime: u64, deadline: Option<u64>) -> StdResult<()> {
 }
 
 pub fn migrate_version(
-
-    set_contract_version(deps.storage, name, version)?;
-
-    Ok(())
-}
-
-#[test]
-fn test_assert_deadline_with_normal() {
-    assert_deadline(5u64, Some(10u64)).unwrap();
-}
+    deps: DepsMut<TerraQuery>,
+    target_contract_version: &str,
+    name: &str,
+    version: &str,
+) -> StdResult<()> {
 
 #[test]
 fn test_assert_deadline_with_expired() {
